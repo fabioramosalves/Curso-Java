5753b987270046683b929b3966140ad3b5c1ab2d
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-02 23:26:55 -0300

    calculo

--- App.java
+++ App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int x = 10;
+        int y = 5;
+
+        int result = x * y;
     }
 }
