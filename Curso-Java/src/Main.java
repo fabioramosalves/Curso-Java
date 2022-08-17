10c48ea7aac3c8d09bee3336e5b150adde220db8
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-17 13:56:27 -0300

    Inclusão metodos de entradas Java

--- Curso-Java/src/Main.java
+++ Curso-Java/src/Main.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
-        int x = 10;
-        int y = 5;
 
-        int result = x * y;
+     //   %f = ponto flutuante
+     //   %d = inteiro
+     //   %s = texto
+     //   %n = quebra de linha
 
-        System.out.println(result);
+        String nome = "Maria";
+        int idade = 31;
+        double renda = 4000.0;
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+
+        Scanner sc = new Scanner(System.in);
+        String s1, s2, s3;
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
+        System.out.println("DADOS DIGITADOS:");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        sc.close();
     }
 }
