1a5ca744b1a2dd92ed293d4c7a061364546e0e05
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-17 14:34:35 -0300

    incluindo classe estática utuil

--- Curso-Java/src/Main.java
+++ Curso-Java/src/Main.java
@@ -9,20 +9,23 @@ public class Main {
      //   %s = texto
      //   %n = quebra de linha
 
-        String nome = "Maria";
-        int idade = 31;
-        double renda = 4000.0;
-        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+       String nome = "Maria";
+       int idade = 31;
+       double renda = 4000.0;
+       System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
 
-        Scanner sc = new Scanner(System.in);
-        String s1, s2, s3;
-        s1 = sc.nextLine();
-        s2 = sc.nextLine();
-        s3 = sc.nextLine();
-        System.out.println("DADOS DIGITADOS:");
-        System.out.println(s1);
-        System.out.println(s2);
-        System.out.println(s3);
-        sc.close();
+       Scanner sc = new Scanner(System.in);
+       String s1, s2, s3;
+       s1 = sc.nextLine();
+       s2 = sc.nextLine();
+       s3 = sc.nextLine();
+       System.out.println("DADOS DIGITADOS:");
+       System.out.println(s1);
+       System.out.println(s2);
+       System.out.println(s3);
+       sc.close();
+
+        System.out.printf("Calculo Aárea %f", Util.CalculoAreaTrapezio(12,6,5));
+
     }
 }
---
+++ Curso-Java/src/Util.java
@@ -0,0 +1,7 @@
+public class Util {
+
+    public static double CalculoAreaTrapezio(int baseA, int altura, int baseB)
+    {
+        return (baseA + baseB)/2 * altura;
+    }   
+}
