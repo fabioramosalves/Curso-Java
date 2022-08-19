3b7258c83640c98676bcfac15bfb1ac9b3b98fde
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-19 13:45:50 -0300

    corrigindo método CalculoAreaTrapezio, alterando geter setter

--- Curso-Java/src/Main.java
+++ Curso-Java/src/Main.java
@@ -3,29 +3,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-
      //   %f = ponto flutuante
      //   %d = inteiro
      //   %s = texto
      //   %n = quebra de linha
 
-       String nome = "Maria";
-       int idade = 31;
-       double renda = 4000.0;
-       System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+      //String nome = "Maria";
+      //int idade = 31;
+      //double renda = 4000.0;
+      //System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
 
-       Scanner sc = new Scanner(System.in);
-       String s1, s2, s3;
-       s1 = sc.nextLine();
-       s2 = sc.nextLine();
-       s3 = sc.nextLine();
-       System.out.println("DADOS DIGITADOS:");
-       System.out.println(s1);
-       System.out.println(s2);
-       System.out.println(s3);
-       sc.close();
+      //Scanner sc = new Scanner(System.in);
+      //String s1, s2, s3;
+      //s1 = sc.nextLine();
+      //s2 = sc.nextLine();
+      //s3 = sc.nextLine();
+      //System.out.println("DADOS DIGITADOS:");
+      //System.out.println(s1);
+      //System.out.println(s2);
+      //System.out.println(s3);
+      //sc.close();
 
-        System.out.printf("Calculo Aárea %f", Util.CalculoAreaTrapezio(12,6,5));
+       var util = new Util();
 
+       util.setAltura(15);
+       util.setBaseA(15);
+       util.setBaseB(15);
+
+        System.out.printf("Calculo Aárea %f", util.CalculoAreaTrapezio());
     }
 }
--- Curso-Java/src/Util.java
+++ Curso-Java/src/Util.java
@@ -1,7 +1,38 @@
 public class Util {
 
-    public static double CalculoAreaTrapezio(int baseA, int altura, int baseB)
-    {
-        return (baseA + baseB)/2 * altura;
-    }   
+    private int baseA;
+    private int baseB;
+    private int altura;
+
+    public void setBaseA(int baseA) {
+        this.baseA = baseA;
+    }
+
+    public void setBaseB(int baseB) {
+        this.baseB = baseB;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+
+    //get
+    public int getBaseA() {
+        return this.baseA;
+    }
+
+    public int getBaseB(int baseB) {
+        return this.baseB;
+    }
+
+    public int getAltura(int altura) {
+        return this.altura;
+    }
+
+    //methods
+
+    public double CalculoAreaTrapezio() {
+        return (baseA + baseB) / 2 * altura;
+    }
 }
