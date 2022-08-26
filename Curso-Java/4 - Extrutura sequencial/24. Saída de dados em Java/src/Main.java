ff4e6379a90af14308200f17396e1a0597a7fe2a
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-26 10:37:03 -0300

    inclusão de metodo de saída de dados

--- Curso-Java/4 - Extrutura sequencial/24. Saída de dados em Java/src/Main.java
+++ Curso-Java/4 - Extrutura sequencial/24. Saída de dados em Java/src/Main.java
@@ -1,69 +1,28 @@
 import java.util.Locale;
-import java.util.Scanner;
-
-//import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-
-     //   %f = ponto flutuante
-     //   %d = inteiro
-     //   %s = texto
-     //   %n = quebra de linha
-
-      String nome = "Maria";
-      int idade = 31;
-      double renda = 4000.0;
-      System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
-
-      Scanner sc = new Scanner(System.in);
-      String s1, s2, s3;
-      s1 = sc.nextLine();
-      s2 = sc.nextLine();
-      s3 = sc.nextLine();
-      System.out.println("DADOS DIGITADOS:");
-      System.out.println(s1);
-      System.out.println(s2);
-      System.out.println(s3);
-      sc.close();
-
-      var util = new Util();
-
-       util.setAltura(15);
-       util.setBaseA(15);
-       util.setBaseB(15);
-
-        System.out.printf("Calculo Aárea %f", util.CalculoAreaTrapezio());
-
-
+        public static void main(String[] args) throws Exception {
+        
         Locale.setDefault(Locale.US);
 
-        Scanner scanner = new Scanner(System.in);
+        // ---------para escrever na tela um texto qualquer-----------
 
-        double x;
+        // Com quebra de linha
+        System.out.println("Ola Mundo");
 
-        x = scanner.nextDouble();
+        // Sem quebra de linha
+        System.out.println("Bom dia!");
 
-        System.out.printf("Você digitou: %.2f%n", x);
+        // --------Conteúdo de uma variável de um tipo basico
 
-        char _char;
+        int x = 8;
+        double y = 8.6576;
+        System.out.println("Valor: " + x);
+        System.out.println("Valor: " + y);
 
-        _char = scanner.next().charAt(0);
+        // Padrão para controle das casas decimais <"%.2f%n">
 
-        System.out.println("Você digitou: " + _char);
-
-
-        // para ler varios dados na mesma linha utulizar 
-
-
-       String a;
-       int b;
-       double c;
-
-       a = scanner.next();
-       b = scanner.nextInt();
-       c = scanner.nextDouble();
-
-
+        System.out.printf("%.2f%n", y);
+        System.out.printf("%.4f%n", y);
     }
 }
