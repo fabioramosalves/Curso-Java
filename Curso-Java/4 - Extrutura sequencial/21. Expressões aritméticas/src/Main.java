6a57856e574f861ce3196b41818fe362d8c49249
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-26 09:10:46 -0300

    aula 21

--- Curso-Java/4 - Extrutura sequencial/21. Expressões aritméticas/src/Main.java
+++ Curso-Java/4 - Extrutura sequencial/21. Expressões aritméticas/src/Main.java
@@ -1,69 +1,34 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-//import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-     //   %f = ponto flutuante
-     //   %d = inteiro
-     //   %s = texto
-     //   %n = quebra de linha
+    
+      // Operadores aritiméticos
 
-      String nome = "Maria";
-      int idade = 31;
-      double renda = 4000.0;
-      System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+      // + (Adição)
+      // - (subtração)
+      // * (multiplicação)
+      // / (divisão)
+      // % (resto da divisão - "mod")
 
-      Scanner sc = new Scanner(System.in);
-      String s1, s2, s3;
-      s1 = sc.nextLine();
-      s2 = sc.nextLine();
-      s3 = sc.nextLine();
-      System.out.println("DADOS DIGITADOS:");
-      System.out.println(s1);
-      System.out.println(s2);
-      System.out.println(s3);
-      sc.close();
+      // precedência (quem faz primeiro) 
+      // primeiro * / %
+      // segundo + -
 
-      var util = new Util();
+      var result = 2 * 6/3; // result = 4
 
-       util.setAltura(15);
-       util.setBaseA(15);
-       util.setBaseB(15);
+      result = 3 + 2 * 4; // result = 11
 
-        System.out.printf("Calculo Aárea %f", util.CalculoAreaTrapezio());
+      result = (3 + 2) * 4; // result = 20
 
+      result = 60/(3 + 2) * 4; // result = 48
 
-        Locale.setDefault(Locale.US);
+      result = 60/((3 + 2) * 4); // result = 3
 
-        Scanner scanner = new Scanner(System.in);
-
-        double x;
-
-        x = scanner.nextDouble();
-
-        System.out.printf("Você digitou: %.2f%n", x);
-
-        char _char;
-
-        _char = scanner.next().charAt(0);
-
-        System.out.println("Você digitou: " + _char);
-
-
-        // para ler varios dados na mesma linha utulizar 
-
-
-       String a;
-       int b;
-       double c;
-
-       a = scanner.next();
-       b = scanner.nextInt();
-       c = scanner.nextDouble();
-
+      System.out.println("Resultado: " + result);
 
     }
 }
