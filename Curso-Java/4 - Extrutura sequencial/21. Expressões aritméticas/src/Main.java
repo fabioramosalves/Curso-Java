5fa147ffc3364dbb15d0af6626b91cdb5db44bd8
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-26 09:34:29 -0300

    removendo import desnecessário

--- Curso-Java/4 - Extrutura sequencial/21. Expressões aritméticas/src/Main.java
+++ Curso-Java/4 - Extrutura sequencial/21. Expressões aritméticas/src/Main.java
@@ -1,34 +1,29 @@
-import java.util.Locale;
-import java.util.Scanner;
-
-
 public class Main {
     public static void main(String[] args) throws Exception {
 
-    
-      // Operadores aritiméticos
+        // Operadores aritiméticos
 
-      // + (Adição)
-      // - (subtração)
-      // * (multiplicação)
-      // / (divisão)
-      // % (resto da divisão - "mod")
+        // + (Adição)
+        // - (subtração)
+        // * (multiplicação)
+        // / (divisão)
+        // % (resto da divisão - "mod")
 
-      // precedência (quem faz primeiro) 
-      // primeiro * / %
-      // segundo + -
+        // precedência (quem faz primeiro)
+        // primeiro * / %
+        // segundo + -
 
-      var result = 2 * 6/3; // result = 4
+        var result = 2 * 6 / 3; // result = 4
 
-      result = 3 + 2 * 4; // result = 11
+        result = 3 + 2 * 4; // result = 11
 
-      result = (3 + 2) * 4; // result = 20
+        result = (3 + 2) * 4; // result = 20
 
-      result = 60/(3 + 2) * 4; // result = 48
+        result = 60 / (3 + 2) * 4; // result = 48
 
-      result = 60/((3 + 2) * 4); // result = 3
+        result = 60 / ((3 + 2) * 4); // result = 3
 
-      System.out.println("Resultado: " + result);
+        System.out.println("Resultado: " + result);
 
     }
 }
