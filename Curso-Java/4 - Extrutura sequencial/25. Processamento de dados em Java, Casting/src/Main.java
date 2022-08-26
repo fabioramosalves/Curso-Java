0de77106ecca0cd270decfca8cb8b2a7a67439a7
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-26 13:53:22 -0300

    Incluindo aula do processamento de dados

--- Curso-Java/4 - Extrutura sequencial/25. Processamento de dados em Java, Casting/src/Main.java
+++ Curso-Java/4 - Extrutura sequencial/25. Processamento de dados em Java, Casting/src/Main.java
@@ -1,7 +1,60 @@
 public class Main {
     public static void main(String[] args) throws Exception {
 
-     
+        // ---------Processamento de dados em Java------------
 
+        // <variável> = <expressão>
+
+        // Exemplo 1
+
+        int x, y;
+        x = 5;
+        y = 2 * x;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        // Exemplo 2
+
+        int xx;
+        double yy;
+        xx = 5;
+        yy = 2 * xx;
+
+        System.out.println(xx);
+        System.out.println(yy);
+
+        // Exemplo 3
+
+        double area, h, b, B;
+        b = 6.0;
+        h = 5.0;
+        B = 8.0;
+        area = (b + B) / 2 * h;
+
+        System.out.println("Area: " + area);
+
+        //Exemplo 4 casting
+
+        int a, c;
+        double resultado;
+
+        a = 5;
+        c = 2;
+
+        resultado = (double) a / c; //casting double
+
+        System.out.println(resultado);
+
+        // Exemplo 5
+
+        double aa;
+        int bb;
+
+        aa = 5.0;
+        // bb = aa; // O compilador não deixa atribuir uma variável que perderá valor deve se utilizar o casting
+        bb =(int) aa;
+        
+        System.out.println(bb);
     }
 }
