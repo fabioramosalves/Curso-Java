d8bd8dd67ad324e55f8a254e36f2162ce7fdd124
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-26 20:33:14 -0300

    Correção exercicios

--- Curso-Java/5 - Extrutura condicional/37. Exercícios para Iniciantes - PARTE 2/src/Main.java
+++ Curso-Java/5 - Extrutura condicional/37. Exercícios para Iniciantes - PARTE 2/src/Main.java
@@ -1,21 +1,32 @@
 import java.util.Scanner;
 
-import javax.lang.model.util.ElementScanner14;
-
 public class Main {
     public static void main(String[] args) throws Exception {
 
-     // exercicio 1
+        System.out.println("Informe um número!");
 
-     Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-     int number = scanner.nextInt();
+        int number = scanner.nextInt();
 
-     System.out.println(number<0 ? "NEGATIVO" : "NÃO NEGATIVO");
+        // exercicio 1
+        System.out.println(number < 0 ? "NEGATIVO" : "NÃO NEGATIVO");
 
-     
-     
-     scanner.close();
+        // exercicio 2
+        System.out.println(number % 2 == 0 ? "Par" : "Impar");
+
+        // exercicio 3
+        System.out.println("Informe os numeros!");
+        int number1, number2;
+
+        number1 = scanner.nextInt();
+        number2 = scanner.nextInt();
+
+        System.out.println((number1 % number2 == 0 || number2 % number1 == 0) ? "Sao Multiplos" : "Nao sao Multiplos");
+
+        // exercicio 4
+
+        scanner.close();
 
     }
 }
