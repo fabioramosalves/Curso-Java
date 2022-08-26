de18fb12c1fe73ffb2c0ae4457b59a132fa43612
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-26 12:37:11 -0300

    incluindo exercicios de fixação

--- Curso-Java/4 - Extrutura sequencial/24. Saída de dados em Java/src/Main.java
+++ Curso-Java/4 - Extrutura sequencial/24. Saída de dados em Java/src/Main.java
@@ -22,7 +22,6 @@ public class Main {
 
         System.out.printf("%.2f%n", y);
 
-        Locale.setDefault(Locale.US);
         System.out.printf("%.4f%n", y);
 
         // para concatenar vários elementos em um mesmo comando de escrita
@@ -43,5 +42,30 @@ public class Main {
         double renda = 4050.55;
 
         System.out.printf("%s tem %d anos e ganha R$ %.2f reais %n", nome, idade, renda);
+
+        //////////// Exercicio de fixação////////////////////////
+
+        String product1 = "Computer";
+        String product2 = "Office desk";
+
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+
+        double price1 = 2100.0;
+        double price2 = 650.50;
+        double measure = 53.234567;
+
+        System.out.println("Products:");
+        System.out.printf("%s, which price is $ %.2f %n", product1, price1);
+        System.out.printf("%s, which price is $ %.2f %n", product2, price2);
+
+        System.out.printf("Record: %d old years, code %d and gender: %s %n", age, code, gender);
+
+        System.out.printf("Measue with eight decimal places:: %.8f %n", measure);
+        System.out.printf("Rouded (three decimal places): %.3f %n", measure);
+
+        Locale.setDefault(Locale.US);
+        System.out.printf("US decimal point: %.3f %n", measure);
     }
 }
