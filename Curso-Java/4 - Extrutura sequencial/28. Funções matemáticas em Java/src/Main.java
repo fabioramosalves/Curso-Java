d87f673a01bc390bcfd7f26a7233d6a1a0fd6dec
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-26 15:02:58 -0300

    Incluindo funções matematicas em expressões maiores

--- Curso-Java/4 - Extrutura sequencial/28. Funções matemáticas em Java/src/Main.java
+++ Curso-Java/4 - Extrutura sequencial/28. Funções matemáticas em Java/src/Main.java
@@ -29,5 +29,21 @@ public class Main {
 
         System.out.println("Valor absoluto de " + y + " = " + A);
         System.out.println("Valor absoluto de " + z + " = " + B);
+
+        // Incluindo funções em expressões maiores
+
+        double delta, b, a, c, x1, x2;
+
+        a = 3.0;
+        b = 7.0;
+        c = 5.0;
+
+        delta = (Math.pow(b, 2.0) - 4) * a * c;
+
+        x1 = (-b + Math.sqrt(delta)) / (2.0 * a);
+        x2 = (-b - Math.sqrt(delta)) / (2.0 * a);
+
+        System.out.println("X1: " + x1);
+        System.out.println("X2: " + x2);
     }
 }
