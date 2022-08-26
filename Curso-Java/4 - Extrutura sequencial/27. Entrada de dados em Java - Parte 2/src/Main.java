e606cbbdfc8dc22c146ab4f2cb867b8134edd080
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-26 14:26:23 -0300

    Entradas de dados em java parte2

--- Curso-Java/4 - Extrutura sequencial/27. Entrada de dados em Java - Parte 2/src/Main.java
+++ Curso-Java/4 - Extrutura sequencial/27. Entrada de dados em Java - Parte 2/src/Main.java
@@ -1,69 +1,27 @@
-import java.util.Locale;
 import java.util.Scanner;
-
-//import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-     //   %f = ponto flutuante
-     //   %d = inteiro
-     //   %s = texto
-     //   %n = quebra de linha
+        Scanner sc = new Scanner(System.in);
 
-      String nome = "Maria";
-      int idade = 31;
-      double renda = 4000.0;
-      System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+        String s1, s2, s3;
+        int x;
 
-      Scanner sc = new Scanner(System.in);
-      String s1, s2, s3;
-      s1 = sc.nextLine();
-      s2 = sc.nextLine();
-      s3 = sc.nextLine();
-      System.out.println("DADOS DIGITADOS:");
-      System.out.println(s1);
-      System.out.println(s2);
-      System.out.println(s3);
-      sc.close();
+        x = sc.nextInt();
+        sc.nextLine(); // massete deve ser incluído um nexline para seguir
+        
+        s1 = sc.nextLine(); // Ler até o final da linha
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
 
-      var util = new Util();
+        System.out.println("Dados Digitados:");
 
-       util.setAltura(15);
-       util.setBaseA(15);
-       util.setBaseB(15);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
 
-        System.out.printf("Calculo Aárea %f", util.CalculoAreaTrapezio());
-
-
-        Locale.setDefault(Locale.US);
-
-        Scanner scanner = new Scanner(System.in);
-
-        double x;
-
-        x = scanner.nextDouble();
-
-        System.out.printf("Você digitou: %.2f%n", x);
-
-        char _char;
-
-        _char = scanner.next().charAt(0);
-
-        System.out.println("Você digitou: " + _char);
-
-
-        // para ler varios dados na mesma linha utulizar 
-
-
-       String a;
-       int b;
-       double c;
-
-       a = scanner.next();
-       b = scanner.nextInt();
-       c = scanner.nextDouble();
-
+        sc.close();
 
     }
 }
