7b2801375c06f32a62e93d0d55b6218f837b4916
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-26 18:51:49 -0300

    Estrutura condicional

--- Curso-Java/5 - Extrutura condicional/35. Estrutura condicional (if-else)/src/Main.java
+++ Curso-Java/5 - Extrutura condicional/35. Estrutura condicional (if-else)/src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
+   public static void main(String[] args) throws Exception {
 
+      /*
+       * Estrutura condicional
+       * 
+       */
+      int x;
+      Scanner sc = new Scanner(System.in);
+      x = sc.nextInt();
 
-    }
+      if (x >= 0 && x <= 12)
+         System.out.println("Bom dia!");
+
+      if (x > 12 && x < 18)
+         System.out.println("Boa tarde!");
+
+      if (x >= 18 && x <= 24)
+         System.out.println("Boa noite!");
+
+      if (x < 0 || x > 24)
+         System.out.println("Dado inválido!");
+
+      sc.close();
+
+   }
 }
