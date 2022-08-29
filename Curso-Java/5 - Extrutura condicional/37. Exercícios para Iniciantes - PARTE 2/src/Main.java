657d8badfb643b8a3b73d215728caa5785d43d50
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-29 18:31:26 -0300

    correção exercício 7

--- Curso-Java/5 - Extrutura condicional/37. Exercícios para Iniciantes - PARTE 2/src/Main.java
+++ Curso-Java/5 - Extrutura condicional/37. Exercícios para Iniciantes - PARTE 2/src/Main.java
@@ -122,18 +122,20 @@ public class Main {
               salary = scanner.nextDouble();
 
               if (salary <= 2000)
-                     System.out.println("Isento");
-              else if (salary <= 3000) {
+                     tax = 0;
+              else if (salary <= 3000)
                      tax = (salary - 2000) * 0.8;
-                     System.out.printf("R$ %.2f", tax);
-              } else if (salary <= 4500) {
+
+              else if (salary <= 4500)
                      tax = (salary - 3000.0) * 0.18 + 1000.0 * 0.08;
-                     System.out.printf("R$ %.2f", tax);
-              }
-              else {
+
+              else
                      tax = (salary - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
-                     System.out.printf("R$ %.2f", tax);
-              }
+
+              if (tax == 0.0)
+                     System.out.println("Isento");
+              else
+                     System.out.printf("R$ %.2f%n", tax);
 
               scanner.close();
        }
