fad7bd7840aa623f6a9a6da7086f26193f0723a2
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-29 20:30:34 -0300

    condicional ternaria

--- Curso-Java/5 - Extrutura condicional/40. Expressão condicional ternária/src/Main.java
+++ Curso-Java/5 - Extrutura condicional/40. Expressão condicional ternária/src/Main.java
@@ -1,69 +1,22 @@
-import java.util.Locale;
 import java.util.Scanner;
-
-//import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-     //   %f = ponto flutuante
-     //   %d = inteiro
-     //   %s = texto
-     //   %n = quebra de linha
+        //expressão condicional ternária
 
-      String nome = "Maria";
-      int idade = 31;
-      double renda = 4000.0;
-      System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+        //(condição) ? valor_se_verdadeiro : valor_se_falso
 
-      Scanner sc = new Scanner(System.in);
-      String s1, s2, s3;
-      s1 = sc.nextLine();
-      s2 = sc.nextLine();
-      s3 = sc.nextLine();
-      System.out.println("DADOS DIGITADOS:");
-      System.out.println(s1);
-      System.out.println(s2);
-      System.out.println(s3);
-      sc.close();
-
-      var util = new Util();
-
-       util.setAltura(15);
-       util.setBaseA(15);
-       util.setBaseB(15);
-
-        System.out.printf("Calculo Aárea %f", util.CalculoAreaTrapezio());
-
-
-        Locale.setDefault(Locale.US);
 
         Scanner scanner = new Scanner(System.in);
 
-        double x;
+        double pricing = 34.5;
 
-        x = scanner.nextDouble();
+        double desc = (pricing < 20) ? pricing * 0.1 : pricing * 0.05;
 
-        System.out.printf("Você digitou: %.2f%n", x);
+        System.out.println(desc);
 
-        char _char;
-
-        _char = scanner.next().charAt(0);
-
-        System.out.println("Você digitou: " + _char);
-
-
-        // para ler varios dados na mesma linha utulizar 
-
-
-       String a;
-       int b;
-       double c;
-
-       a = scanner.next();
-       b = scanner.nextInt();
-       c = scanner.nextDouble();
-
-
+        scanner.close();
+     
     }
 }
