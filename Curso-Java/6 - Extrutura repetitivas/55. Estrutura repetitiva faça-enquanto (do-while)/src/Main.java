8feefd3ba9b6adfb31ba3e1c911cad1a56ffa83d
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-30 13:38:02 -0300

    Estrutura repetitiva do-while

--- Curso-Java/6 - Extrutura repetitivas/55. Estrutura repetitiva faça-enquanto (do-while)/src/Main.java
+++ Curso-Java/6 - Extrutura repetitivas/55. Estrutura repetitiva faça-enquanto (do-while)/src/Main.java
@@ -1,69 +1,24 @@
-import java.util.Locale;
 import java.util.Scanner;
-
-//import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-     //   %f = ponto flutuante
-     //   %d = inteiro
-     //   %s = texto
-     //   %n = quebra de linha
-
-      String nome = "Maria";
-      int idade = 31;
-      double renda = 4000.0;
-      System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
-
-      Scanner sc = new Scanner(System.in);
-      String s1, s2, s3;
-      s1 = sc.nextLine();
-      s2 = sc.nextLine();
-      s3 = sc.nextLine();
-      System.out.println("DADOS DIGITADOS:");
-      System.out.println(s1);
-      System.out.println(s2);
-      System.out.println(s3);
-      sc.close();
-
-      var util = new Util();
-
-       util.setAltura(15);
-       util.setBaseA(15);
-       util.setBaseB(15);
-
-        System.out.printf("Calculo Aárea %f", util.CalculoAreaTrapezio());
-
-
-        Locale.setDefault(Locale.US);
-
         Scanner scanner = new Scanner(System.in);
+        char stop;
+        do{
 
-        double x;
+        System.out.println("Digite a temperatura em Celsius: ");
+        double c = scanner.nextDouble();
 
-        x = scanner.nextDouble();
+        System.out.printf("Equivalente em Fahreheit: %.1f%n", 9 * c / 5 + 32);       
 
-        System.out.printf("Você digitou: %.2f%n", x);
+        System.out.println("Deseja repetir (s/n)");
 
-        char _char;
+        stop = scanner.next().charAt(0);
 
-        _char = scanner.next().charAt(0);
+        }while(stop == 's');
 
-        System.out.println("Você digitou: " + _char);
-
-
-        // para ler varios dados na mesma linha utulizar 
-
-
-       String a;
-       int b;
-       double c;
-
-       a = scanner.next();
-       b = scanner.nextInt();
-       c = scanner.nextDouble();
-
-
+        scanner.close();
+     
     }
 }
