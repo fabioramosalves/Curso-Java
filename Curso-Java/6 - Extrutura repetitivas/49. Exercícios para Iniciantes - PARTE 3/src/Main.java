15853c3753d55c47a0c5d9ef74e340d617b20043
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-29 22:57:46 -0300

    Inclusão de exercicio 3

--- Curso-Java/6 - Extrutura repetitivas/49. Exercícios para Iniciantes - PARTE 3/src/Main.java
+++ Curso-Java/6 - Extrutura repetitivas/49. Exercícios para Iniciantes - PARTE 3/src/Main.java
@@ -3,22 +3,70 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-     //exercicio 1
+        // exercicio 1
 
-     Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-     System.out.println("digite a senha");
+        System.out.println("digite a senha");
 
-     int senha = scanner.nextInt();
+        int senha = scanner.nextInt();
 
-     while(senha != 2002){
-        System.out.println("Senha inválida!");
-        senha = scanner.nextInt();    
-     }
+        while (senha != 2002) {
+            System.out.println("Senha inválida!");
+            senha = scanner.nextInt();
+        }
 
-     System.out.println("Acesso Permitido");
+        System.out.println("Acesso Permitido");
 
-     scanner.close();
+        // Exercício 2
+        int x, y;
+        String q = "";
+        x = scanner.nextInt();
+        y = scanner.nextInt();
 
+        while (x != 0 && y != 0) {
+            if (x > 0 && y > 0) {
+                q = "primeiro";
+            } else if (x < 0 && y > 0) {
+                q = "segundo";
+            } else if (x < 0 && y < 0) {
+                q = "terceiro";
+            } else {
+                q = "quarto";
+            }
+
+            System.out.println(q);
+
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+        }
+         //Exercício 3
+
+        int codigo, alcool, gasolina, diesel;
+        alcool = 0;
+        gasolina = 0;
+        diesel = 0;
+
+        codigo = scanner.nextInt();
+
+        while (codigo != 4) {
+            switch (codigo) {
+                case 1:
+                alcool += 1;
+                break;
+                case 2:
+                gasolina += 1;
+                break;
+                case 3:
+                diesel += 1;
+                break;
+            }
+
+            codigo = scanner.nextInt();
+        }
+
+        System.out.printf("Muito Obrigado! %n Alcool: %d%n Gasolina: %d%n Diesel: %d%n", alcool, gasolina,diesel);
+
+        scanner.close();
     }
 }
