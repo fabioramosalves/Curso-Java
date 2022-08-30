8b4ddb7d3cde98291072d13e3670225208207f89
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-29 21:07:25 -0300

    Escopo de inicialização

--- Curso-Java/5 - Extrutura condicional/41. Escopo e inicialização/src/Main.java
+++ Curso-Java/5 - Extrutura condicional/41. Escopo e inicialização/src/Main.java
@@ -1,7 +1,20 @@
 public class Main {
     public static void main(String[] args) throws Exception {
 
+        // escopo de uma variáve é a região do programa onde a variável é válida, ou
+        // seja onde ela pode ser referenciada.
 
-        
+        // Uma variável não pode ser usada se não for iniciada
+
+        double price = 400.0;
+        double discount;
+
+        if (price < 200)
+            discount = price * 0.1;
+        else
+            discount = 0;
+
+        System.out.println(discount);
+
     }
 }
