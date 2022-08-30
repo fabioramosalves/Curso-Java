99709d64cd7b150fb071bb27d84b7fb986d8b0de
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-29 21:56:16 -0300

    Extrutura repetitiva

--- Curso-Java/6 - Extrutura repetitivas/45. Estrutura repetitiva enquanto (while)/src/Main.java
+++ Curso-Java/6 - Extrutura repetitivas/45. Estrutura repetitiva enquanto (while)/src/Main.java
@@ -1,69 +1,25 @@
-import java.util.Locale;
 import java.util.Scanner;
-
-//import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-     //   %f = ponto flutuante
-     //   %d = inteiro
-     //   %s = texto
-     //   %n = quebra de linha
+     Scanner scanner = new Scanner(System.in);
 
-      String nome = "Maria";
-      int idade = 31;
-      double renda = 4000.0;
-      System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+     System.out.println("Digite um número!");
+     int number = scanner.nextInt();
+     int value = 0;
 
-      Scanner sc = new Scanner(System.in);
-      String s1, s2, s3;
-      s1 = sc.nextLine();
-      s2 = sc.nextLine();
-      s3 = sc.nextLine();
-      System.out.println("DADOS DIGITADOS:");
-      System.out.println(s1);
-      System.out.println(s2);
-      System.out.println(s3);
-      sc.close();
+     while(number != 0){
 
-      var util = new Util();
+        value += number;
+        System.out.println("Digite um número!");
+        number = scanner.nextInt();
+ 
+     }
 
-       util.setAltura(15);
-       util.setBaseA(15);
-       util.setBaseB(15);
+     System.out.println(value);
 
-        System.out.printf("Calculo Aárea %f", util.CalculoAreaTrapezio());
-
-
-        Locale.setDefault(Locale.US);
-
-        Scanner scanner = new Scanner(System.in);
-
-        double x;
-
-        x = scanner.nextDouble();
-
-        System.out.printf("Você digitou: %.2f%n", x);
-
-        char _char;
-
-        _char = scanner.next().charAt(0);
-
-        System.out.println("Você digitou: " + _char);
-
-
-        // para ler varios dados na mesma linha utulizar 
-
-
-       String a;
-       int b;
-       double c;
-
-       a = scanner.next();
-       b = scanner.nextInt();
-       c = scanner.nextDouble();
-
+     scanner.close();
 
     }
 }
