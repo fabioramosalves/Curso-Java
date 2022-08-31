3b6ed1f3bd492ed19213a48726b5e7ff100792c9
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-31 10:22:26 -0300

    Inclusão de metodo sem OOP

--- Curso-Java/8 - Introdução à programação orientada a objeto/64. Resolvendo um problema sem orientação a objetos/src/Main.java
+++ Curso-Java/8 - Introdução à programação orientada a objeto/64. Resolvendo um problema sem orientação a objetos/src/Main.java
@@ -1,13 +1,52 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
 
+        Locale.setDefault(Locale.US);
         // area do triangulo
+        Scanner scanner = new Scanner(System.in);
 
+        double xA, xB, xC, yA, yB, yC;
 
         System.out.println("Enter the messures of triangle X");
+        xA = scanner.nextInt();
+        xB = scanner.nextInt();
+        xC = scanner.nextInt();
+
         System.out.println("Enter the messures of triangle Y");
+        yA = scanner.nextInt();
+        yB = scanner.nextInt();
+        yC = scanner.nextInt();
 
+        double p = (xA + xB + xC) / 2.0;
+        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
 
-    
+        p = (yA + yB + yC) / 2.0;
+        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+        System.out.printf("Triangle X area: %.4f%n", areaX);
+        System.out.printf("Triangle Y area: %.4f%n", areaY);
+
+        MaxArea(areaX, areaY);
+
+        scanner.close();
+
+    }
+
+    public static void MaxArea(double x, double y) {
+        double result;
+        String triangle;
+        if (x > y) {
+            result = x;
+            triangle = "X";
+
+        } else {
+            result = y;
+            triangle = "Y";
+        }
+
+        System.out.printf("Larger area: %s %.4f%n", triangle, result);
     }
 }
