abd3c535f5140d0337c3fef9587154de59f11377
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-08-31 18:09:19 -0300

    Versão tres da classe calculator

--- Curso-Java/8 - Introdução à programação orientada a objeto/71. Membros estáticos - Parte 1/src/application/Program.java
+++ Curso-Java/8 - Introdução à programação orientada a objeto/71. Membros estáticos - Parte 1/src/application/Program.java
@@ -1,26 +1,21 @@
 package application;
-
 import java.util.Locale;
 import java.util.Scanner;
-
 import util.Calculator;
 
 public class Program {
-
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
-        Calculator calculator = new Calculator();
-
         System.out.println("Enter radius: ");
 
         double radius = scanner.nextDouble();
 
-        double circumference = calculator.circumference(radius);
+        double circumference = Calculator.circumference(radius);
 
-        double volume = calculator.volume(radius);
+        double volume = Calculator.volume(radius);
 
         System.out.printf("Circunferência: %.2f%n", circumference);
         System.out.printf("Volume: %.2f%n", volume);
@@ -29,6 +24,4 @@ public class Program {
         scanner.close();
 
     }
-
-  
 }
--- Curso-Java/8 - Introdução à programação orientada a objeto/71. Membros estáticos - Parte 1/src/util/Calculator.java
+++ Curso-Java/8 - Introdução à programação orientada a objeto/71. Membros estáticos - Parte 1/src/util/Calculator.java
@@ -1,11 +1,12 @@
 package util;
 
 public class Calculator {
-    public double volume(double radius) {
+
+    public static double volume(double radius) {
         return 4 * Math.PI * Math.pow(radius, 3) / 3;
     }
 
-    public double circumference(double radius) {
+    public static double circumference(double radius) {
         return Math.PI * radius * 2;
     }
 }
